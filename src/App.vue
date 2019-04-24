70cf545c5007607565473ade87da46a96264ad40
xue <947441187@qq.com>
2019-04-24 17:25:24 +0800

    项目开始主要完成事件： 1.创建项目添加到GIT中 2.导入jquery以及bootstrap 3.创建导航栏组件

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
   </div>
 
 </template>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script>
 
 export default {
